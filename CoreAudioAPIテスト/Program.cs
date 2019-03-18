358c132b25c4bceb1d35d7a065cb1cc068946de8
shigobu <shigobu@shigobu.com>
2019-03-18 11:45:17 +0900

    音量操作できた。

--- CoreAudioAPIテスト/Program.cs
+++ CoreAudioAPIテスト/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoreAudioApi;
 
 namespace CoreAudioAPIテスト
 {
@@ -10,6 +11,22 @@ namespace CoreAudioAPIテスト
     {
         static void Main(string[] args)
         {
+            //自分自身のプロセスを取得する
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetCurrentProcess();
+            var pid = p.Id;
+
+            MMDevice device;
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+            device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            AudioSessionManager sessionManager = device.AudioSessionManager;
+            foreach (var item in sessionManager.Sessions)
+            {
+                if (item.ProcessID != (uint)pid)
+                {
+                    item.SimpleAudioVolume.MasterVolume = 1f;
+                }                
+            }
+            //device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)10 / 100.0f);
         }
     }
 }
